7edbbb40f400b98e5b7f78da1aa7186a86c55ea0
KAVIAHKA IROSHAN <kavishkairoshan54@gmail.com>
2025-12-31 19:19:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'pak.html'));
+res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/admin', (req, res) => {
-res.sendFile(path.join(__dirname, 'vasa.html'));
+res.sendFile(path.join(__dirname, 'admin.html'));
 });
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
