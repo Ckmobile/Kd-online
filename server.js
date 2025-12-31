3d216ab5f79caf94c1277cfba9ac38de0aa10cce
KAVIAHKA IROSHAN <kavishkairoshan54@gmail.com>
2025-12-31 19:35:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'index.html'));
+res.sendFile(path.join(__dirname, 'pak.html'));
 });
 app.get('/admin', (req, res) => {
 res.sendFile(path.join(__dirname, 'admin.html'));
